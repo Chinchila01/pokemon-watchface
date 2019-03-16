bee2f2b6422c06e2c789efa31bbd561d631fcee7
Filipe Correia <gmangreatness@gmail.com>
2019-03-16 21:14:48 +0000

    Typo.

--- install-pebble-sdk.sh
+++ install-pebble-sdk.sh
@@ -5,7 +5,7 @@ tar xvf pebble-sdk-4.5-linux64.tar.bz2
 sudo pip install virtualenv
 
 cd pebble-sdk-4.5-linux64
-virtualenv --no-sie-packages .env
+virtualenv --no-site-packages .env
 source .env/bin/activate
 pip install -r requirements.txt
 deactivate
