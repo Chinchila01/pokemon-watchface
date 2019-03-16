9b9891d171472b31383baa482a8d2f602eef39f8
Filipe Correia <gmangreatness@gmail.com>
2019-03-16 21:11:47 +0000

    Added apt get packages as addons. Distro changed to trusty.

--- install-pebble-sdk.sh
+++ install-pebble-sdk.sh
@@ -2,7 +2,6 @@
 set -ex
 wget https://developer.rebble.io/s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-4.5-linux64.tar.bz2
 tar xvf pebble-sdk-4.5-linux64.tar.bz2
-sudo apt-get install python-pip python2.7-dev
 sudo pip install virtualenv
 
 cd pebble-sdk-4.5-linux64
