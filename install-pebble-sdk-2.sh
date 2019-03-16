e381feaba0306e0b00ab9f2edb03e156b4fbd476
Filipe Correia <gmangreatness@gmail.com>
2019-03-16 21:23:37 +0000

    .

--- install-pebble-sdk-2.sh
+++ install-pebble-sdk-2.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -ex
-
 pebble sdk install https://github.com/aveao/PebbleArchive/raw/master/SDKCores/sdk-core-4.3.tar.bz2
-cd ~/.pebble-sdk
-touch NO_TRACKING
-cd $TRAVIS_BUILD_DIR
+touch ~/.pebble-sdk/NO_TRACKING
